8822a8e32fdf02c5bc9eb3f875fc780895e8a046
Tahoe Schrader <tahoeschrader@gmail.com>
2024-02-03 01:30:34 -0500

    add some helix configurations for lsp and debuggers

--- modules/common/applications/helix.nix
+++ modules/common/applications/helix.nix
@@ -1,6 +1,34 @@
 { config, pkgs, lib, ... }: {
   options.helix.enable = lib.mkEnableOption "Helix code editor.";
   config = lib.mkIf config.helix.enable {
-    home.packages = with pkgs; [ helix ];
+    programs.helix = {
+      enable = true;
+      languages = {
+        language = [
+          # Change default language server for python
+          {
+            name = "python";
+            language-servers = ["pyright"];
+          }
+        ];
+      };
+    };
+
+    # Add language servers/debuggers
+    home.packages = with pkgs; [
+      llvmPackages.lldb # debugger
+      texlab # latex and bibtex lsp
+      yaml-language-server
+      taplo # toml lsp
+      pyright # python lsp
+      nil # nix lsp
+      marksman # markdown lsp
+      gopls # go lsp
+      ansible-language-server
+      terraform-ls
+      nodePackages.bash-language-server
+      vscode-langservers-extracted
+      dockerfile-language-server-nodejs
+    ];
   };
 }
