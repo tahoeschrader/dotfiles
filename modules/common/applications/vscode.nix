53a83c206771960784496dc45c0476558eefe260
Tahoe Schrader <tahoeschrader@gmail.com>
2024-02-05 00:33:26 -0500

    improve comments and keep extensions im actually interested in
    there are too many extensions and i shouldnt go crazy added everything
    since i want to learn terminal development. only keep the bare minimum
    that is useful in a jam, and stuff that only works on vscode. a lot of
    it seems to not work with rust at the moment so left them as comments

--- modules/common/applications/vscode.nix
+++ modules/common/applications/vscode.nix
@@ -7,10 +7,21 @@
     programs.vscode = {
       enable = true;
       extensions = with pkgs.vscode-extensions; [
-        #vadimcn.vscode-lldb
+        #vadimcn.vscode-lldb # tons of build errors -- need for debugging rust and other languages
+        #ms-python.vscode-pylance # can't run due to Unfree -- do I need this if I have pyright globally installed?
+        #luquedaniel.renpy-language # doesn't exist -- needed for Ren'Py Visual Novel engine
+        #platformio.platformio-ide # doesn't exist -- useful for embedded/arduino programming
         rust-lang.rust-analyzer
         tamasfe.even-better-toml
-        #platformio
+        ms-python.python
+        james-yu.latex-workshop
+        ms-kubernetes-tools.vscode-kubernetes-tools
+        ms-azuretools.vscode-docker
+        redhat.vscode-yaml
+        bierner.markdown-mermaid
+        yzhang.markdown-all-in-one
+        golang.go
+        bbenoist.nix
       ];
     };
   };
