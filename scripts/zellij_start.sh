08dc7b0ddfcf91edc9766babf96d24fe0e2c9be8
Tahoe Schrader <tahoeschrader@gmail.com>
2024-12-06 13:41:15 +0900

    if zellij session is already alive, kill it first
    do this becaues zellij doesnt persist like tmux. could get around this by having tmux be in a zellij
    pane but that feels wrong

--- scripts/zellij_start.sh
+++ scripts/zellij_start.sh
@@ -4,7 +4,7 @@ SESSION_NAME="$1"
 LAYOUT_FILE="$2"
 
 if zellij list-sessions | grep -q "$SESSION_NAME"; then
-  zellij attach "$SESSION_NAME"
+  zellij kill-session "$SESSION_NAME"
 else
   zellij -s "$SESSION_NAME" -l "$LAYOUT_FILE"
 fi
