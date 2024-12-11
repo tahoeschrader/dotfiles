b51a3cac71c79be1a8300dc59fda3c6a1c2649ed
Tahoe Schrader <tahoeschrader@gmail.com>
2024-12-11 14:06:43 +0900

    after deleting session, open it

--- scripts/zellij_start.sh
+++ scripts/zellij_start.sh
@@ -4,7 +4,8 @@ SESSION_NAME="$1"
 LAYOUT_FILE="$2"
 
 if zellij list-sessions | grep -q "$SESSION_NAME"; then
-  zellij kill-session "$SESSION_NAME"
+  zellij delete-session "$SESSION_NAME"
+  zellij -s "$SESSION_NAME" -l "$LAYOUT_FILE"
 else
   zellij -s "$SESSION_NAME" -l "$LAYOUT_FILE"
 fi
