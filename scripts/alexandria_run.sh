7560ec7d3604fe1d3bc96c7882e654983abbd896
Tahoe Schrader <tahoeschrader@gmail.com>
2025-01-11 13:39:03 +0900

    clean alexandria-run and layout to better run cargo build and test

--- scripts/alexandria_run.sh
+++ scripts/alexandria_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Run alias (currently not used/working for anything)
-DATABASE_URL=postgresql://tester:password@127.0.0.1:5432/testing cargo test
+command="$1"
+path="$2"
+DATABASE_URL=postgresql://tester:password@127.0.0.1:5432/testing cargo "$command" --manifest-path "$path"
